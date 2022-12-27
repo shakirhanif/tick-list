07445e7da93095d4a70ed65eb41e6b9e95dffda6
Shakir Hanif <shakirhanif@hotmail.com>
2022-12-27 21:27:29 +0500

    app complete

---
+++ components/CreateArea.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef, useState } from "react";
+
+function CreateArea(props) {
+  const refOne = useRef(null);
+  useEffect(() => {
+    document.addEventListener("click", (e) => {
+      if (!refOne.current.contains(e.target)) {
+        setFocus(false);
+      }
+    });
+  }, []);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [focus, setFocus] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+    setFocus(false);
+  }
+
+  function focusHandler() {
+    setFocus(true);
+  }
+
+  return (
+    <div ref={refOne}>
+      <form className="create-note">
+        {focus ? (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+            autoFocus
+          />
+        ) : null}
+
+        <textarea
+          ref={refOne}
+          onClick={focusHandler}
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={focus ? "3" : "1"}
+        />
+        {focus ? <button onClick={submitNote}>+</button> : null}
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
